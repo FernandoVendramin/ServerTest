349d2eae8c072dbc73884827acce4208dad2cc0d
fernando.vendramin <fernando.vendramin@dextra-sw.com>
2020-10-16 14:36:41 -0300

    grid actions

--- src/ServerTest.Ui.Angular/src/app/pages/client/client-form-devexpress/client-form-devexpress.component.ts
+++ src/ServerTest.Ui.Angular/src/app/pages/client/client-form-devexpress/client-form-devexpress.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faEdit, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { Client } from 'src/app/models/client';
 import { ClientAddress } from 'src/app/models/clientaddress';
+import { ClientPhone } from 'src/app/models/clientphone';
 import { ClientType } from 'src/app/models/clienttype';
 import { ClientService } from 'src/app/services/client.service';
 import { ClientTypeService } from 'src/app/services/clienttype.service';
@@ -17,13 +19,17 @@ import { StateChange } from 'src/app/utils/statechange';
 export class ClientFormDevexpressComponent implements OnInit {
 
   faPlus = faPlus;
+  faEdit = faEdit;
 
   client: Client = null;
   clientTypes: ClientType[] = null;
   clientAddressesDeleted: ClientAddress[] = [];
+  clientPhoneForm: FormGroup;
+
   id: number;
 
   constructor(
+    private formBuilder: FormBuilder,
     private clientService: ClientService,
     private clientTypeService: ClientTypeService,
     private toastrService: ToastrService,
@@ -80,8 +86,41 @@ export class ClientFormDevexpressComponent implements OnInit {
     console.log(e.data);
   }
 
-  openPhoneModal(content) {
+  openPhoneModal(content, phone) {
+    this.clientPhoneForm = this.formBuilder.group({
+      id: [phone?.id ?? null, Validators.required],
+      clientId: [phone?.clientId ?? null, Validators.required],
+      phoneNumber: [phone?.phoneNumber ?? null, [Validators.required, Validators.minLength(8)]],
+      editionDateTime: [phone?.editionDateTime ?? null, Validators.required],
+      stateChange: [phone?.stateChange != null ? StateChange.Updated :  StateChange.Inserted, Validators.required],
+    });
+
     this.modalService.open(content, { centered: true });
+  }
+
+  savePhoneModal() {
+    const myPhone: ClientPhone = {
+      id: this.clientPhoneForm.value.id,
+      editionDateTime: new Date(),
+      stateChange: this.clientPhoneForm.value.stateChange,
+      clientId: this.client.id,
+      client: null,
+      phoneNumber: this.clientPhoneForm.value.phoneNumber
+    };
+
+    if (myPhone.stateChange == StateChange.Updated)
+    {
+      this.client.phones.forEach(phone => {
+        if (phone.id == myPhone.id) {
+          phone.phoneNumber = myPhone.phoneNumber;
+        }
+      });
+    }
+    else{
+      this.client.phones.push(myPhone);
+    }
+
+    this.modalService.dismissAll();
   }
 
   onRowRemovedAddress(e) {
