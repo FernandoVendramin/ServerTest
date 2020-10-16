4eb74cb57f0b2511cac43abb84bcc36dfd8e4fc3
fernando.vendramin <fernando.vendramin@dextra-sw.com>
2020-10-16 12:13:53 -0300

    gzip compression

--- src/ServerTest.Api/Startup.cs
+++ src/ServerTest.Api/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ServerTest.Infra;
+using System.IO.Compression;
 
 namespace ServerTest
 {
@@ -39,6 +41,14 @@ namespace ServerTest
                     .AllowAnyMethod()
                 );
             });
+
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest);
+
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +59,7 @@ namespace ServerTest
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseResponseCompression();
             app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
